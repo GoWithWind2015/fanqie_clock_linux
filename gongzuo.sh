f3943e29b7d55925fa720682347ef030a179e46d
GoWithWind2015 <2560053475@qq.com>
2018-04-24 21:29:29 +0800

    第一次提交相关更改，原始程序

---
+++ gongzuo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#第一步 激活go-for-it界面
+com.github.jmoerman.go-for-it&
+ wmctrl -a it!  -e 0,1540,730,350,200
+#第二步，播放闹
+mocp -p /home/hu/Pro_self/fanqie_todo/gongzuo.wav
+
---
+++ xiuxi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#第一步 激活go-for-it界面
+mocp -s
+com.github.jmoerman.go-for-it&
+#第二步，播放闹
+
